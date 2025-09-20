ed4e2479c6d23856df886dfbae56fc5d43800ae4
Jinansh28 <jinansh2807@gmail.com>
2025-09-20 10:55:08 +0530

    19 September 2025

--- Leetcode/3634_MinimumRemovalsToBalanceAnArray.cpp
+++ Leetcode/3634_MinimumRemovalsToBalanceAnArray.cpp
@@ -8,22 +8,16 @@ using namespace std;
 int minRemoval(vector<int>& nums, int k) {
     if(nums.size()==1) return 0;
     sort(nums.begin(),nums.end());
-    if(((long long)nums[0]*k)>=nums[nums.size()-1]) return 0;
-    int n=nums.size(),ans=n,c=0,a=n;
-    for(int i=0;i<n;i++){
-        c=i;
-        for(int j=n-1;j>=i;j--){
-            if((long long)nums[i]*k>=nums[j]){
-                ans=min(ans,c);
-            }
-            else{
-                c++;
-            }
+    int n = nums.size();
+    if(((long long)nums[0]*k)>=nums[n-1]) return 0;
+    int i = 0, maxlen = 0,ans = 0;
+    for(int j = 0; j< n ;j++){
+        if(((long long)nums[i]*k)<nums[j]) i++;
+        if(((long long)nums[i]*k)>=nums[j]){
+            maxlen = max(maxlen,j-i);
         }
-        a=min(a,c);
     }
-    if(ans!=n) return ans;
-    return a;    
+    return n-maxlen;
 }
 
 int main(){
